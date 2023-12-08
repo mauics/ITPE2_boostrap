88b473fd1934635173093cdcea0ad5d636023295
ChipsInSane <charlesandrew0123@gmail.com>
2023-12-08 17:48:54 +0800

    Made the user info and edit user info

---
+++ wews/UserInfo/UInfo.js
@@ -0,0 +1,80 @@
+const storedUserInfo = JSON.parse(localStorage.getItem('userInfo')) || {};
+        const editModal = document.getElementById('edit-modal');
+        const pictureInput = document.getElementById('edit-picture');
+        const fullNameInput = document.getElementById('edit-fullName');
+        const emailInput = document.getElementById('edit-email');
+        const currentAddressInput = document.getElementById('edit-currentAddress');
+        const genderInput = document.getElementById('edit-gender');
+        const dobInput = document.getElementById('edit-dob');
+        const currentPasswordInput = document.getElementById('edit-currentPassword');
+        const newPasswordInput = document.getElementById('edit-newPassword');
+        const confirmNewPasswordInput = document.getElementById('edit-confirmNewPassword');
+
+        document.getElementById('user-picture').src = storedUserInfo.picture || 'default.jpg';
+        document.getElementById('fullName').innerText = storedUserInfo.fullName || 'John Doe';
+        document.getElementById('email').innerText = storedUserInfo.email || 'john.doe@example.com';
+        document.getElementById('currentAddress').innerText = storedUserInfo.currentAddress || '123 Main St, Cityville';
+        document.getElementById('gender').innerText = storedUserInfo.gender || 'Male';
+        document.getElementById('dob').innerText = storedUserInfo.dob || 'January 1, 1990';
+
+        function openEditModal() {
+            pictureInput.value = ''; // Clear the file input
+            fullNameInput.value = storedUserInfo.fullName || '';
+            emailInput.value = storedUserInfo.email || '';
+            currentAddressInput.value = storedUserInfo.currentAddress || '';
+            genderInput.value = storedUserInfo.gender || '';
+            dobInput.value = storedUserInfo.dob || '';
+            currentPasswordInput.value = ''; // Clear the current password input
+            newPasswordInput.value = ''; // Clear the new password input
+            confirmNewPasswordInput.value = ''; // Clear the confirm new password input
+
+            editModal.style.display = 'flex';
+        }
+
+        function saveUserInformation() {
+            // Check if the current password matches the stored password
+            if (currentPasswordInput.value !== storedUserInfo.password) {
+                alert('Incorrect current password. Changes not saved.');
+                return;
+            }
+
+            // Check if the new password and confirm new password match
+            if (newPasswordInput.value !== confirmNewPasswordInput.value) {
+                alert('New password and confirm new password do not match. Changes not saved.');
+                return;
+            }
+
+            const newUserInfo = {
+                picture: getImageDataUrl(pictureInput.files[0]) || storedUserInfo.picture || 'default.jpg',
+                fullName: fullNameInput.value,
+                email: emailInput.value,
+                currentAddress: currentAddressInput.value,
+                gender: genderInput.value,
+                dob: dobInput.value,
+                password: newPasswordInput.value || storedUserInfo.password
+            };
+
+            localStorage.setItem('userInfo', JSON.stringify(newUserInfo));
+
+            // Update the displayed information
+            document.getElementById('user-picture').src = newUserInfo.picture;
+            document.getElementById('fullName').innerText = newUserInfo.fullName;
+            document.getElementById('email').innerText = newUserInfo.email;
+            document.getElementById('currentAddress').innerText = newUserInfo.currentAddress;
+            document.getElementById('gender').innerText = newUserInfo.gender;
+            document.getElementById('dob').innerText = newUserInfo.dob;
+
+            // Close the modal
+            editModal.style.display = 'none';
+        }
+
+        function getImageDataUrl(file) {
+            if (!file) return null;
+            const reader = new FileReader();
+            return new Promise((resolve) => {
+                reader.onloadend = () => {
+                    resolve(reader.result);
+                };
+                reader.readAsDataURL(file);
+            });
+        }
